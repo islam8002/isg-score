61accf636c7b81c33ce85ffcca6431f96d93715c
islam8002 <gabississo@gmail.com>
2025-12-23 21:43:29 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import fetch from "node-fetch";
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+app.use(cors());
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, "public")));
+
+const PORT = process.env.PORT || 3000;
+
+// ضع API KEY هنا
+const API_KEY = "PUT_YOUR_API_KEY_HERE";
+
+app.get("/live", async (req, res) => {
+  try {
+    const response = await fetch(
+      "https://v3.football.api-sports.io/fixtures?live=all",
+      { headers: { "x-apisports-key": API_KEY } }
+    );
+    const data = await response.json();
+    res.json(data.response);
+  } catch (err) {
+    res.status(500).json({ error: "API ERROR" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`⚽ Server running on port ${PORT}`);
+});
